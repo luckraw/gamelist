ab76a07426a7d02125c732a9882237fc19c90ba7
Lucas <krawdev@gmail.com>
2023-05-10 16:18:53 -0300

    Database seed, Game repository

---
+++ src/main/java/com/example/demo/dto/GameDTO.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto;
+
+public class GameDTO {
+
+    private Long Id;
+    private String title;
+
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+
+    private String shortDescription;
+
+    private String longDescription;
+
+}
